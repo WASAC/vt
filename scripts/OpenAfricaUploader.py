b71cb24db4d753313fc6b15c06125677227faddd
Jin IGARASHI <wushilan0623@gmail.com>
2020-11-15 22:06:03 +0900

    modified ckan uploader for overwriting existing resource

--- scripts/OpenAfricaUploader.py
+++ scripts/OpenAfricaUploader.py
@@ -5,13 +5,22 @@ import requests
 
 class OpanAfricaUploader(object):
   def __init__(self, api_key):
+    """Constructor
+
+    Args:
+        api_key (string): CKAN api key
+    """
     self.data_portal = 'https://africaopendata.org'
-    # upload api
-    self.api_url = self.data_portal + '/api/action/resource_create'
     self.APIKEY = api_key
     self.ckan = ckanapi.RemoteCKAN(self.data_portal, apikey=self.APIKEY)
 
   def create_package(self, url, title):
+    """create new package if it does not exist yet.
+
+    Args:
+        url (str): the url of package eg. https://open.africa/dataset/{package url}
+        title (str): the title of package
+    """
     package_name = url
     package_title = title
     try:
@@ -27,20 +36,65 @@ class OpanAfricaUploader(object):
         else:
             raise
 
-  def upload_datasets(self, path, description):
-    filename = os.path.basename(path)
-    extension = os.path.splitext(filename)[1][1:].lower()
-    
-    print ('Creating "{filename}"'.format(**locals()))
+  def resource_create(self, data, path, api="/api/action/resource_create"):
+    """create new resource, or update existing resource
+
+    Args:
+        data (object): data for creating resource. data must contain package_id, name, format, description. If you overwrite existing resource, id also must be included.
+        path (str): file path for uploading
+        api (str, optional): API url for creating or updating. Defaults to "/api/action/resource_create". If you want to update, please specify url for "/api/action/resource_update"
+    """
+    self.api_url = self.data_portal + api
+    print ('Creating "{}"'.format(data['name']))
     r = requests.post(self.api_url,
-                      data={'package_id': self.package['id'],
-                            'name': filename,
-                            'format': extension,
-                            'description': description
-                            },
+                      data=data,
                       headers={'Authorization': self.APIKEY},
                       files=[('upload', open(path, 'rb'))])
 
     if r.status_code != 200:
         print ('Error while creating resource: {0}'.format(r.content))
-    print ('Uploaded "{filename}" successfully'.format(**locals()))
+    else:
+      print ('Uploaded "{}" successfully'.format(data['name']))
+
+  def resource_update(self, data, path):
+    """update existing resource
+
+    Args:
+        data (object): data for creating resource. data must contain id, package_id, name, format, description.
+        path (str): file path for uploading
+    """
+    self.resource_create(data, path, "/api/action/resource_update")
+
+  def upload_datasets(self, path, description):
+    """upload datasets under the package
+
+    Args:
+        path (str): file path for uploading
+        description (str): description for the dataset
+    """
+    filename = os.path.basename(path)
+    extension = os.path.splitext(filename)[1][1:].lower()
+    
+    data = {
+      'package_id': self.package['id'],
+      'name': filename,
+      'format': extension,
+      'description': description
+    }
+
+    resources = self.package['resources']
+    if len(resources) > 0:
+      target_resource = None
+      for resource in reversed(resources):
+        if filename == resource['name']:
+          target_resource = resource
+          break
+
+      if target_resource == None:
+        self.resource_create(data, path)
+      else:
+        print ('Resource "{}" already exists, it will be overwritten'.format(target_resource['name']))
+        data['id'] = target_resource['id']
+        self.resource_update(data, path)
+    else:
+      self.resource_create(data, path)
